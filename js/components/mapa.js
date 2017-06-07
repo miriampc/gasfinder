edf489d9d421095a3ad52457d16a025742373666
miriam <miiriampc@gmail.com>
2017-06-07 00:41:36 -0500

    creando marcadores y capturando la ubicacion actual en el mapa

--- js/components/mapa.js
+++ js/components/mapa.js
@@ -1,7 +1,44 @@
-$(_=>{
-  var map = new GMaps({
-      el: '#map',
-      lat: -12.043333,
-      lng: -77.028333
-    });
-})
+// $(_=>{
+
+  //
+  // var map = new GMaps({
+  //     el: '#map',
+  //     lat: -12.043333,
+  //     lng: -77.028333
+  //   });
+    function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 8,
+          center: {lat: -12.043333, lng: -77.028333}
+        });
+
+    var geocoder = new google.maps.Geocoder;
+      var infowindow = new google.maps.InfoWindow;
+
+    geocodeLatLng(geocoder, map, infowindow);
+
+    function geocodeLatLng(geocoder, map, infowindow) {
+        // var input = "Avenida del Ejército, Distrito de Lima"
+        // var latlngStr = input.split(',', 2);
+        var latlng = {lat: -12.1093606, lng: -77.0565751};
+        geocoder.geocode({'location': latlng}, function(results, status) {
+          if (status === 'OK') {
+            if (results[1]) {
+              console.log(latlng);
+              map.setZoom(11);
+              var marker = new google.maps.Marker({
+                position: latlng,
+                map: map
+              });
+              infowindow.setContent(results[1].formatted_address);
+              infowindow.open(map, marker);
+            } else {
+              window.alert('No results found');
+            }
+          } else {
+            window.alert('Geocoder failed due to: ' + status);
+          }
+        });
+      }
+}
+// })
--- js/components/station-details.js
+++ js/components/station-details.js
@@ -13,19 +13,54 @@ const StationDetails = (update) => {
   detail.append(address);
   detail.append(gas90);
 
+  var latitud, longitud;
   $(_=>{
-    var map = new GMaps({
+    //Crea el mapa
+    const map = new GMaps({
         div: '#map',
-        lat: state.selectedStation.lat,//-12.1191427,
-        lng: state.selectedStation.long// -77.0349046
+        lat: state.selectedStation.lat,
+        lng: state.selectedStation.long
       });
-
+    //Añade marcador de la direccion de la estacion seleccionada
     map.addMarker({
       lat: state.selectedStation.lat,
       lng: state.selectedStation.long,
       title: state.selectedStation.name,
-      click: function(e) {
-        alert('You clicked in this marker');
+      infoWindow: {
+        content: '<strong>Estacion de gas:</strong><p>'+state.selectedStation.address+'</p>'
+      }
+    });
+
+  //Determina la ubicación actual
+    GMaps.geolocate({
+      success: function(position) {
+        latitud = position.coords.latitude;
+        longitud = position.coords.longitude;
+        map.setCenter(latitud, longitud);
+
+      //Funcion que añade marcador de la ubicacion actual
+        map.addMarker({
+          lat: latitud,
+          lng: longitud,
+          infoWindow: {
+            content: '<strong>Tu ubicacion:</strong><p>lima</p>'
+          }
+        });
+      //Funcion para trazar la ruta
+        map.drawRoute({
+          origin: [latitud, longitud],
+          destination: [state.selectedStation.lat, state.selectedStation.long],
+          travelMode: 'driving',
+          strokeColor: '#131540',
+          strokeOpacity: 0.6,
+          strokeWeight: 6
+        });
+      },
+      error: function(error) {
+        alert('Geolocalización fallada: '+error.message);
+      },
+      not_supported: function() {
+        alert("Tu navegador no soporta la API geolocation");
       }
     });
   })
